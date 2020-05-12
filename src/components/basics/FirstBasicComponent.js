8433ed55edea48329f3f650d898b083123e2fa9b
Pedro Santos <phsantosti@gmail.com>
2020-05-12 18:42:41 -0300

    Apredendo Componentes no React.

--- src/components/basics/FirstBasicComponent.js
+++ src/components/basics/FirstBasicComponent.js
@@ -1,3 +1,12 @@
+import React from "react";
+
 export default function FirstBasicComponent() {
-    return 'Primeiro Componente';
+    const title = 'Primeiro Componente';
+    const message = 'Seja bem-vindo(a)';
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{message}</p>
+        </div>
+    );
 }
